b3ac9292775d62b39483d79d47fb70943d65a067
jallier <justin.hallier+git@gmail.com>
2017-05-06 13:50:10 +1200

    Working without routenames

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var fs = require('fs');
+var Promise = require('bluebird');
 var Client = require('node-rest-client').Client;
+Promise.promisifyAll(Client);
 
 var _api = "https://api.at.govt.nz/v2/gtfs/";
 var _tripID = "trips/tripId/";
@@ -25,17 +27,7 @@ console.log(args);
 client.registerMethod("stopTimes", _api + "stopTimes/stopId/" + _stopId, "GET");
 client.registerMethod("tripID", _api + _tripID + "${tripID}", "GET");
 
-// test = "";
-// client.methods.tripID(args, function(data, response){
-//   // This function run when the api returns some data.
-//   test = data;
-//   console.log("data", test);
-// });
-
-function getRouteName(times, tripID, callback){
-  // console.log(times);
-  // console.log(tripID);
-  // console.log("this is a test");
+function getRouteName(tripID, callback){
   args = {
     headers: {
       "Ocp-Apim-Subscription-Key": _key
@@ -44,17 +36,10 @@ function getRouteName(times, tripID, callback){
       "tripID": tripID
     }
   };
-
   client.methods.tripID(args, function(d, r){
     //call this function when route data returned
-    // console.log(d.response[0].route_id)
-    console.log(shared_counter);
-    shared_counter -= 1;
-    times[tripID].route=d.response[0].route_id;
-    if(shared_counter<=0){
-      callback();
-    }
-    // console.log(times);
+    // console.log(d['response']);
+    callback(d['response'][0]['route_id']);
   });
 }
 var shared_counter = 0;
@@ -69,21 +54,21 @@ client.methods.stopTimes(args, function(data, response) {
   timeSinceMidnight = (date.getHours() * 60 * 60) + (date.getMinutes() * 60) + (date.getSeconds()); //in seconds
   // console.log(timeSinceMidnight);
   len = data.length;
-  shared_counter = data.length;
+  shared_counter = 0;
   for (i = 0; i < len; i++) {
     arrival_time = data[i]['arrival_time_seconds'];
     id = data[i]['trip_id'];
     if (arrival_time >= timeSinceMidnight && arrival_time <= (timeSinceMidnight + _timeWindow)) {
+      shared_counter++;
       times[id] = {"arrival_time": arrival_time, "route": ""};
+      console.log(times[id]);
       // args.path.tripID = id;
-      // console.log(args);
-      console.log(shared_counter);
-      getRouteName(times, id, function(){
-        console.log(times);
-      });
     }
   }
-  console.log("Loop complete", times);
+  for (var key in times){
+    // getRouteName()
+  }
+  // console.log("Loop complete", times);
 });
 
 var server = http.createServer(function(req, res) { //req is readable stream that emits data events for each incoming piece of data.
