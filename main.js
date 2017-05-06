5d791a4d6206ad6426f815626b2eea816009f404
jallier <justin.hallier+git@gmail.com>
2017-05-06 15:02:38 +1200

    Working with routenames

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-var Promise = require('bluebird');
 var Client = require('node-rest-client').Client;
-Promise.promisifyAll(Client);
 
 var _api = "https://api.at.govt.nz/v2/gtfs/";
 var _tripID = "trips/tripId/";
@@ -18,58 +16,72 @@ var args = {
   headers: {
     "Ocp-Apim-Subscription-Key": _key
   },
-  path:{
+  path: {
     "tripID": "14277052711-20170420151239_v53.21"
   }
 };
 console.log(args);
 
-client.registerMethod("stopTimes", _api + "stopTimes/stopId/" + _stopId, "GET");
+client.registerMethod("getStopTimes", _api + "stopTimes/stopId/" + _stopId, "GET");
 client.registerMethod("tripID", _api + _tripID + "${tripID}", "GET");
 
-function getRouteName(tripID, callback){
+function getRouteName(tripID, callback) {
   args = {
     headers: {
       "Ocp-Apim-Subscription-Key": _key
     },
-    path:{
+    path: {
       "tripID": tripID
     }
   };
-  client.methods.tripID(args, function(d, r){
+  client.methods.tripID(args, function(d, r) {
     //call this function when route data returned
-    // console.log(d['response']);
-    callback(d['response'][0]['route_id']);
+    callback(tripID, d['response'][0]['route_id']);
   });
 }
-var shared_counter = 0;
-client.methods.stopTimes(args, function(data, response) {
-  // parsed response body as js object
-  // console.log(data['response'][0]);
-  console.log("Received all data; running loop");
-  data = data['response'];
-  times = {};
-  timeSinceMidnight = 0;
-  date = new Date();
-  timeSinceMidnight = (date.getHours() * 60 * 60) + (date.getMinutes() * 60) + (date.getSeconds()); //in seconds
-  // console.log(timeSinceMidnight);
-  len = data.length;
-  shared_counter = 0;
-  for (i = 0; i < len; i++) {
-    arrival_time = data[i]['arrival_time_seconds'];
-    id = data[i]['trip_id'];
-    if (arrival_time >= timeSinceMidnight && arrival_time <= (timeSinceMidnight + _timeWindow)) {
-      shared_counter++;
-      times[id] = {"arrival_time": arrival_time, "route": ""};
-      console.log(times[id]);
-      // args.path.tripID = id;
+
+function assignRoutes(times, callback) {
+  counter = Object.keys(times).length;
+  for (var key in times) {
+    getRouteName(key, function(trip, routeName) {
+      times[trip].route = routeName;
+      counter--;
+      if (counter <= 0) {
+        callback(times);
+      }
+    })
+  }
+}
+
+function getAllData() {
+  client.methods.getStopTimes(args, function(data, response) {
+    // parsed response body as js object
+    console.log("Received all data; running loop");
+    data = data['response'];
+    times = {};
+    timeSinceMidnight = 0;
+    date = new Date();
+    timeSinceMidnight = (date.getHours() * 60 * 60) + (date.getMinutes() * 60) + (date.getSeconds()); //in seconds
+    // console.log(timeSinceMidnight);
+    len = data.length;
+    for (i = 0; i < len; i++) {
+      arrival_time = data[i]['arrival_time_seconds'];
+      id = data[i]['trip_id'];
+      if (arrival_time >= timeSinceMidnight && arrival_time <= (timeSinceMidnight + _timeWindow)) {
+        times[id] = {
+          "arrival_time": arrival_time,
+          "route": ""
+        };
+      }
     }
-  }
-  for (var key in times){
-    // getRouteName()
-  }
-  // console.log("Loop complete", times);
-});
+    assignRoutes(times, function(nextTimes) {
+      console.log(nextTimes);
+    })
+    // console.log("Loop complete", times);
+  });
+}
+
+getAllData();
 
 var server = http.createServer(function(req, res) { //req is readable stream that emits data events for each incoming piece of data.
   res.writeHead(200, {
